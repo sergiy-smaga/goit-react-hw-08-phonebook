e470ffe005fd2637abcf12dd216a837d692dd594
sergiy-smaga <seregasmaga@gmail.com>
2022-09-02 13:53:25 +0300

    styles added

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -37,29 +37,33 @@ export const ContactForm = () => {
 
   return (
     <StyledForm onSubmit={handleSubmit}>
-      <label htmlFor={nameId}>Name</label>
-      <input
-        onChange={handleChange}
-        id={nameId}
-        value={name}
-        type="text"
-        name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
-      />
+      <div>
+        <label htmlFor={nameId}>Name</label>
+        <input
+          onChange={handleChange}
+          id={nameId}
+          value={name}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor={numberId}>Number</label>
+        <input
+          onChange={handleChange}
+          id={numberId}
+          type="tel"
+          value={number}
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </div>
 
-      <label htmlFor={numberId}>Number</label>
-      <input
-        onChange={handleChange}
-        id={numberId}
-        type="tel"
-        value={number}
-        name="number"
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-        required
-      />
       <button type="submit">Add contact</button>
     </StyledForm>
   );
--- src/components/ListElement/StyledListElement.jsx
+++ src/components/ListElement/StyledListElement.jsx
@@ -4,10 +4,27 @@ export const StyledLi = styled.li`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  p {
-    margin-bottom: 10px;
+  margin: 10px 0;
+  padding: 20px;
+  background-color: #c7bea2;
+  border-radius: 5px;
+  transition: ease-in 100ms;
+  &:hover {
+    background-color: #e5dcc3;
   }
   button {
-    margin-left: 15px;
+    display: block;
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #9a9483;
+    background-color: #9a9483;
+    color: #e5dcc3;
+    cursor: pointer;
+    transition: ease-in 200ms;
+    :hover {
+      border: 1px solid #000;
+      background-color: #e5dcc3;
+      color: #9a9483;
+    }
   }
 `;
